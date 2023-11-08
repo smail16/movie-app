b52a016cc9a6353ac6957ccbb61208ea1fa94c34
Ismail Fourati <119300453+smail16@users.noreply.github.com>
2023-11-08 20:06:54 +0100

    m

--- src/App.js
+++ src/App.js
@@ -10,26 +10,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 const moviedata = [
   {
     id: Math.random(),
-    posteurl: "/av.jpg",
-    title: "Avengers",
-    rate: "4",
-    date: "April 2018",
-    trailer: "https://www.youtube.com/embed/hA6hldpSTF8",
-    description:
-      "Les Avengers étaient une équipe d'individus extraordinaires, dotés de super-pouvoirs ou de caractéristiques particulières. Le but de l'équipe était de protéger la stabilité mondiale contre les menaces intérieures ou extraterrestres",
-  },
-  {
-    id: Math.random(),
-    posteurl: "/sw.jpg",
-    title: "Star wars",
-    rate: "5",
-    date: "December 2019",
-    trailer: "https://www.youtube.com/embed/sGbxmsDFVnE",
-    description:
-      "The film was announced after The Walt Disney Company's acquisition of Lucasfilm in October 2012. The film is the first Star Wars film to not extensively involve franchise creator George Lucas, who only served as a creative consultant in the early stages of production",
-  },
-  {
-    id: Math.random(),
     posteurl: "/sw.jpg",
     title: "Star wars",
     rate: "5",
